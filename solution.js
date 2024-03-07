5a4f4b0e2ad42251ab50e4a7681cc4c60f6bad5c
Alexander Stefanov <46712355+netmasterbg@users.noreply.github.com>
2024-03-07 17:04:48 +0200

    Fixed...I hope so :D

--- solution.js
+++ solution.js
@@ -1,26 +1,3 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
-
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
-    }
-}
-
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
-
-function cleanData(e) {
-    changeContent('search-form-content');
-}
 document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
 
 function fillSearchForm(e) {
@@ -58,4 +35,28 @@ function findRoom(e) {
     console.log(reservation);
     changeContent('guest-details-form-content');
 
+}
+
+let reservation =
+{
+    startDate: null,
+    endDate: null,
+    guestsCount: 0,
+    roomType: null,
+    name: null,
+    phone: null,
+    email: null
+}
+
+function changeContent(className) {
+    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
+    if( document.querySelector(`.${className}`) != null){
+    document.querySelector(`.${className}`).classList.remove('hidden');
+    }
+}
+
+document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+
+function cleanData(e) {
+    changeContent('search-form-content');
 }
